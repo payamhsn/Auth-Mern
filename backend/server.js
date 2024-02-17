c83bdd8973a95bc657362632d71d81b470c6c368
Payam <p.hoseini.ch@gmail.com>
2024-02-17 20:28:03 +0330

    Create first route and handle requests that comes to that route.

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
 
 const port = process.env.PORT || 5001;
 
 const app = express();
+
+app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("hello");
