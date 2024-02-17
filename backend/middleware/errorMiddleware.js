265e30fe0e6b8dfef4992e67f50889c55a99b79f
Payam <p.hoseini.ch@gmail.com>
2024-02-17 21:14:19 +0330

    Create all routes and controller functions.

---
+++ backend/middleware/errorMiddleware.js
@@ -0,0 +1,27 @@
+//
+// Baraye test kardane middleware, age dar postman, response 404 befrestim, age in  middleware ha bashan
+// khode matne error ro mibinim vali age nabashan ye plain html mibinim be onvane error.
+
+const notFound = (req, res, next) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+};
+
+const errorHandler = (err, req, res, next) => {
+  let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  let message = err.message;
+
+  // If Mongoose not found error, set to 404 and change message
+  if (err.name === "CastError" && err.kind === "ObjectId") {
+    statusCode = 404;
+    message = "Resource not found";
+  }
+
+  res.status(statusCode).json({
+    message: message,
+    stack: process.env.NODE_ENV === "production" ? null : err.stack,
+  });
+};
+
+export { notFound, errorHandler };
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
 
@@ -13,5 +14,8 @@ app.use("/api/users", userRoutes);
 app.get("/", (req, res) => {
   res.send("hello");
 });
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
