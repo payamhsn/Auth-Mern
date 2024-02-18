abf066aea6576207de96f0650856d0fd2923b439
Payam <p.hoseini.ch@gmail.com>
2024-02-18 19:06:01 +0330

    Complete user authentication functionality and compare users password is userModel code.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,7 +7,22 @@ import generateToken from "../utils/generateToken.js";
 // @access  Public
 
 const authUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "auth" });
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id);
+
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password");
+  }
 });
 
 // @desc    Register a new user
