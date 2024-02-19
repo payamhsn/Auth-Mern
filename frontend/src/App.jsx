8ae329ead171e909a3ff5848cb0ac084933149dd
Payam <p.hoseini.ch@gmail.com>
2024-02-19 20:22:58 +0330

    Implement react toastify.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,19 @@
-import React from "react";
-import Header from "./components/Header";
 import { Container } from "react-bootstrap";
 import { Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Header from "./components/Header";
 
-export default function App() {
+const App = () => {
   return (
     <>
       <Header />
+      <ToastContainer />
       <Container className="my-2">
         <Outlet />
       </Container>
     </>
   );
-}
+};
+
+export default App;
